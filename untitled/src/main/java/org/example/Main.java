060dae6b9df66150331eeb7748c266432e831c12
Asaf Ahmed <asafahmed@Asafs-Mac-mini.local>
2024-07-24 14:55:37 -0500

    mock

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.w3c.dom.ls.LSException;
+
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int[] nums = new int[] {1,2,4,6};
+    }
+
+
+
+
+
+
+
+
+    }
+
+
